3916c2c4300512a03f17d11b7bea5ceee91ef4f0
Lucas Chae <hi@lucaschae.com>
2022-05-24 19:42:06 +0900

    Initial Commit

---
+++ iFilterCities/iFilterCitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFilterCitiesApp.swift
+//  iFilterCities
+//
+//  Created by Lucas Chae on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct iFilterCitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
