5d22afc1cd02eca241c1ec57d063d760953ab4f4
Lucas Chae <hi@lucaschae.com>
2022-07-14 19:11:30 +0900

    Function to pick station with most record if there are multiple stations for the same city

--- iFilterCities/Manager/StationFilter.swift
+++ iFilterCities/Manager/StationFilter.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 func StationFilter(cities: [City], stations: [Station]) -> [City] {
 
@@ -30,21 +31,64 @@ func StationFilter(cities: [City], stations: [Station]) -> [City] {
     for var city in cities {
         // Tokyo
         var foundStation = false
+        var stationWiththisCityName = [Station]()
+        var mostEffectiveStation: Station
         for station in stations {
             // Loop through station list for Tokyo
 //            if city.city == station.name.en {
-            if station.name.en.contains(city.city!) {
-                city.station = station.id
-                finalOutput.append(city)
-                foundStation = true
-                city.hourlyUrl = "https://bulk.meteostat.net/v2/daily/\(city.station ?? "000000").csv.gz"
-                print(city.hourlyUrl!)
-                print(city.city)
-                print(city.country)
-                break
+
+            if station.name.en.localizedCaseInsensitiveContains(city.city!) && station.inventory.daily.start != nil && station.inventory.daily.end != nil {
+                let cityCoordinates = CLLocation(latitude: city.lat ?? 0, longitude: city.lng ?? 0)
+                let stationCoordinates = CLLocation(latitude: station.location.latitude, longitude: station.location.longitude)
+                let distance = Double(cityCoordinates.distance(from: stationCoordinates))
                 
+                print("city CO: \(cityCoordinates)")
+                print("station CO: \(stationCoordinates)")
+                print("distance is: \(distance)")
+                print("city before distance filter: \(city.city)")
+                
+                if(distance < 50000) {
+                    stationWiththisCityName.append(station)
+                    foundStation = true
+                    print(city.city)
+                    print(city.country)
+                }
+                
+
+
+//                city.station = station.id
+//                finalOutput.append(city)
+//                foundStation = true
+//                city.hourlyUrl = "https://bulk.meteostat.net/v2/daily/\(city.station ?? "000000").csv.gz"
+//                print(city.hourlyUrl!)
+//                print(city.city)
+//                print(city.country)
+//                break
             }
         }
+        if foundStation == true {
+            var stationWithMostRecord: Station?
+            var finalStationYear: Int = -1
+            for station in stationWiththisCityName {
+                let startYear = Int(station.inventory.daily.start!.prefix(4))!
+                print("Start year: \(startYear)")
+                let endYear = Int(station.inventory.daily.end!.prefix(4))!
+                print("End year: \(endYear)")
+                let thisStationYear = endYear-startYear
+                
+                if thisStationYear > finalStationYear {
+                    finalStationYear = thisStationYear
+                    stationWithMostRecord = station
+                }
+            }
+            print(stationWithMostRecord)
+            city.station = stationWithMostRecord!.id
+            finalOutput.append(city)
+        
+            
+        }
+        
+        
         if foundStation == false {
             city.station = "Station not found"
             finalOutput.append(city)
--- iFilterCities/Model/Station.swift
+++ iFilterCities/Model/Station.swift
@@ -10,16 +10,47 @@ import Foundation
 struct Station: Codable {
     let id: String
     let name: Name
+    let location: StationLocation
+    let inventory: Inventory
 //    let inventory: Inventory?
     
     struct Name: Codable {
         let en: String
     }
     
+    struct StationLocation: Codable {
+        let latitude: Double
+        let longitude: Double
+    }
+    
+    struct Inventory: Codable {
+        let daily: DailyRecord
+    }
+    
+    struct DailyRecord: Codable {
+        let start: String?
+        let end: String?
+    }
+    
 //    struct Inventory: Codable {
 //        let daily: String?
 //        let monthly: String?
 //    }
+    
+//
+//    "inventory": {
+//        "model": {
+//            "start": "2021-07-14",
+//            "end": "2022-06-02"
+//        },
+//        "hourly": {
+//            "start": "2020-08-10",
+//            "end": "2022-05-23"
+//        },
+//        "daily": {
+//            "start": "2020-08-11",
+//            "end": "2022-05-21"
+//        },
     
 }
 
